b9e737570e416cd7061340f183ac0437c75b8120
Guanwen Wang <gai25@wildcats.unh.edu>
2016-08-27 15:21:38 -0400

    Add files via upload
    Start with Hello World

---
+++ End.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/ruby
+
+puts "This is main Ruby Program"
+
+END {
+    puts "Terminating Ruby Program"
+}
+
+BEGIN {
+    puts "Initializing Ruby Program"
+}
+
+name = "Bryan Bickford" # comments
+# comments ignore me
+
+=begin
+    comments
+=end
---
+++ HereDocument.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/ruby -w
+# -*- coding : utf-8 -*-
+
+print <<EOF
+    first method to creat a here document 。
+    one more line
+EOF
+
+print <<"EOF";                # same with the one above
+    second way to creat a here document 。
+    one more line
+EOF
+
+print <<`EOC`                 # execute commands
+    echo hi there
+    echo lo there
+EOC
+
+print <<"foo", <<"bar"        # you can pile them up 
+    I said foo.
+foo
+    I said bar.
+bar
