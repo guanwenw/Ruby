82c0a6c572eed5271c371362c0687f849c76addf
Guanwen Wang <gai25@wildcats.unh.edu>
2016-08-30 22:39:44 -0500

    Add files via upload

---
+++ Case.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+# -*- coding: UTF-8 -*-
+
+$age =  5
+case $age
+when 0 .. 2
+    puts "0-2 is baby"
+when 3 .. 6
+    puts "3-6 is kid"
+when 7 .. 12
+    puts "7-12 is child"
+when 13 .. 18
+    puts "13-18 is teenager"
+else
+    puts "Other"
+end
