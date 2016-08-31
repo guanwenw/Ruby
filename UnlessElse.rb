dd38aa91aa36462dc7a85ef2dc84340e3f514f04
Guanwen Wang <gai25@wildcats.unh.edu>
2016-08-30 22:36:27 -0500

    Update UnlessElse.rb

--- UnlessElse.rb
+++ UnlessElse.rb
@@ -1,11 +1,17 @@
 #!/usr/bin/ruby
 
-$debug=1
-print "debug\n" if $debug
-
 x=1
 unless x>2
    puts "x is less than 2"
  else
   puts "x is greater than 2"
 end
+
+
+$var =  1
+print "1 -- print this line\n" if $var
+print "2 -- Don't print this line \n" unless $var
+
+
+$var = false
+print "3 -- print this line\n" unless $var
